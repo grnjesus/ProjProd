6df01892794137a4fe830ffa7c5af4056f56ba21
grnjesus <grnjesus@gmail.com>
2021-12-01 03:27:14 +0300

    Добавьте файлы проекта.

---
+++ ProjProd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjProd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
